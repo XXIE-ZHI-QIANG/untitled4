91188be4c075f090d84e8523807f91e5c91ba191
xie <xie@xuexi.com>
2022-07-28 16:31:44 +0800

    master-1

--- src/com/xzq/h/Hello.java
+++ src/com/xzq/h/Hello.java
@@ -7,6 +7,7 @@ public class Hello {
         System.out.println("aniuniu");
         System.out.println("awwq");
         System.out.println("aawwqq");
+        System.out.println("asdsadqweq");
 
     }
 }
