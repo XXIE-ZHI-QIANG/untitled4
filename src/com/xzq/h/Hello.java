7f88079a6aa7400ae64bf1ab080b0349db27e36f
xie <xie@xuexi.com>
2022-07-28 12:08:13 +0800

    hop commit

--- src/com/xzq/h/Hello.java
+++ src/com/xzq/h/Hello.java
@@ -3,6 +3,7 @@ package com.xzq.h;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("xx");
+        System.out.println("asda");
         System.out.println("aniuniu");
     }
 }
