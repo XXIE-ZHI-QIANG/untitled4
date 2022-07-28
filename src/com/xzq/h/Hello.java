245f45503f99d2434aa71ee3d8b048e68a6203a4
xie <xie@xuexi.com>
2022-07-28 12:10:45 +0800

    hop-1

--- src/com/xzq/h/Hello.java
+++ src/com/xzq/h/Hello.java
@@ -2,8 +2,9 @@ package com.xzq.h;
 
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("sdadda");
         System.out.println("xx");
-        System.out.println("asda");
         System.out.println("aniuniu");
+
     }
 }
